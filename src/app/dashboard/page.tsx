867d209d7296aa4f5b09010250dde3ddf00ac313
anurag <anuragabcr@gmail.com>
2023-10-08 23:22:48 +0530

    prisma & user setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import prismadb from "@/db";
 
-const Dashboard = () => {
+const Dashboard = async () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  const dbUser = await prismadb.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
 
   return <div>Dashboard</div>;
 };
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,13 +1,29 @@
 import { TRPCError } from "@trpc/server";
 import { publicProcedure, router } from "./trpc";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import prismadb from "@/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await prismadb.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      await prismadb.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
 
     return { success: true };
   }),
