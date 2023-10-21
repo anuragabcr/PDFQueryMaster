3aa152cc7952521e7f32c0ceb9bb5ee12a7d96a3
anurag <anuragabcr@gmail.com>
2023-10-22 00:07:48 +0530

    bug fix

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 import prismadb from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
@@ -10,90 +11,116 @@ import { PLANS } from "@/config/stripe";
 
 const f = createUploadthing();
 
-export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      const { getUser } = getKindeServerSession();
-      const user = getUser();
+const middleware = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-      if (!user || !user.id) throw new Error("Unauthorized");
+  if (!user || !user.id) throw new Error("Unauthorized");
 
-      const subscriptionPlan = await getUserSubscriptionPlan();
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-      return { userId: user.id, subscriptionPlan };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      const newFile = await prismadb.file.create({
+  return { subscriptionPlan, userId: user.id };
+};
+
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    key: string;
+    name: string;
+    url: string;
+  };
+}) => {
+  const isFileExist = await prismadb.file.findFirst({
+    where: {
+      key: file.key,
+    },
+  });
+
+  if (isFileExist) return;
+
+  const createdFile = await prismadb.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: "PROCESSING",
+    },
+  });
+
+  try {
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    );
+
+    const blob = await response.blob();
+
+    const loader = new PDFLoader(blob);
+
+    const pageLevelDocs = await loader.load();
+
+    const pagesAmt = pageLevelDocs.length;
+
+    const { subscriptionPlan } = metadata;
+    const { isSubscribed } = subscriptionPlan;
+
+    const isProExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+    const isFreeExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+      await prismadb.file.update({
         data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: "PROCESSING",
+          uploadStatus: "FAILED",
+        },
+        where: {
+          id: createdFile.id,
         },
       });
-      try {
-        const response = await fetch(
-          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-        );
-        const blob = await response.blob();
+    }
 
-        const loader = new PDFLoader(blob);
+    // vectorize and index entire document
+    const pineconeIndex = pinecone.Index("quill");
 
-        const pageLevelDocs = await loader.load();
-        const pageAmt = pageLevelDocs.length;
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
 
-        const { subscriptionPlan } = metadata;
-        const { isSubscribed } = subscriptionPlan;
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+    });
 
-        const isProExceeded =
-          pageAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
-        const isFreeExceeded =
-          pageAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+    await prismadb.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (err) {
+    await prismadb.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
-        if (
-          (isSubscribed && isProExceeded) ||
-          (!isSubscribed && isFreeExceeded)
-        ) {
-          await prismadb.file.update({
-            data: {
-              uploadStatus: "FAILED",
-            },
-            where: {
-              id: newFile.id,
-            },
-          });
-        }
-
-        const pineconeIndex = pinecone.Index("query-master");
-        const embeddings = new OpenAIEmbeddings({
-          openAIApiKey: process.env.OPENAI_API_KEY,
-        });
-
-        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-          pineconeIndex,
-        });
-        await prismadb.file.update({
-          where: {
-            id: newFile.id,
-          },
-          data: {
-            uploadStatus: "SUCCESS",
-          },
-        });
-      } catch (error) {
-        console.log(error);
-
-        await prismadb.file.update({
-          where: {
-            id: newFile.id,
-          },
-          data: {
-            uploadStatus: "FAILED",
-          },
-        });
-      }
-    }),
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
