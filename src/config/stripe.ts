319095c1b84edf4c52869367947453832caa0c9c
anurag <anuragabcr@gmail.com>
2023-10-21 22:49:37 +0530

    stripe payment

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 20,
+    pagesPerPdf: 10,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 100,
+      priceIds: {
+        test: "price_1O3ij0SIXa6Yph2QzTJGdflu",
+        production: "",
+      },
+    },
+  },
+];
